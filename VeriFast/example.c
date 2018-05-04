02af0a7e641013c0ac5c4c2432905c625701991c
Stephan Lipp <sphlpp91@gmail.com>
2018-05-04 12:44:59 +0200

    added cblock struct

--- VeriFast/example.c
+++ VeriFast/example.c
@@ -1,6 +1,40 @@
 #include "stdlib.h"
 
-bool contains(char *arr, int len, char c)
+struct cblock {
+  int pos;
+  int len;
+  char *arr;
+};
+
+/*@
+predicate cblock(struct cblock *cblock) =
+  cblock->pos |-> ?pos &*&
+  cblock->len |-> ?len &*&
+  cblock->arr |-> ?arr &*&
+  chars(arr, len, ?list) &*&
+  malloc_block(arr, len) &*&
+  malloc_block_cblock(cblock);
+@*/
+
+struct cblock *create_cblock(int len)
+  //@ requires len >= 0;
+  //@ ensures cblock(result);
+{
+  struct cblock *cblock = malloc(sizeof(struct cblock));
+  if (cblock == 0) { abort(); }
+  char *arr = malloc(len);
+  if (arr == 0) {
+    free(cblock);
+    abort();
+  }
+  cblock->pos = 0;
+  cblock->len = len;
+  cblock->arr = arr;
+  //@ close cblock(cblock);
+  return cblock;
+}
+
+bool array_contains(char *arr, int len, char c)
   //@ requires chars(arr, len, ?list);
   /*@ ensures chars(arr, len, list) &*&
         (len == 0) ?
@@ -13,11 +47,30 @@ bool contains(char *arr, int len, char c)
   if (len > 0) {
     //@ open chars(arr, len, list);
     bool cmp = (*arr == c);
-    bool tmp = contains(arr + 1, len - 1, c);
+    bool tmp = array_contains(arr + 1, len - 1, c);
     res = (cmp || tmp);
     //@ close chars(arr, len, list);
   }
   return res;
+}
+
+bool cblock_contains(struct cblock *cblock, char c)
+  //@ requires cblock(cblock);
+  //@ ensures cblock(cblock);
+{
+  //@ open cblock(cblock);
+  bool res = array_contains(cblock->arr, cblock->len, c);
+  //@ close cblock(cblock);
+  return res;
+}
+
+void cblock_dispose(struct cblock *cblock)
+  //@ requires cblock(cblock);
+  //@ ensures true;
+{
+  //@ open cblock(cblock);
+  free(cblock->arr);
+  free(cblock);
 }
 
 int main()
@@ -28,7 +81,7 @@ int main()
   char *arr = malloc(len);
   if (arr == 0) { abort(); }
   // ...
-  bool res = contains(arr, len, 'x');
+  bool res = array_contains(arr, len, 'x');
   free(arr);
   return 0;
 }
